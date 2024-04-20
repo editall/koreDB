60ca7fb5005cebccb976e8503a7cc5f330c22d0d
easylogic <cyberuls@gmail.com>
2024-04-20 14:57:17 +0900

    add headless test

--- packages/kore-db/vitest.config.ts
+++ packages/kore-db/vitest.config.ts
@@ -1,11 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    browser: {
+      enabled: true,
+      name: "chrome",
+      headless: true,
+    },
+
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./test/setup.ts",
   },
 });
