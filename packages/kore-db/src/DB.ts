cfb598cbccf163653b972a57fb00942134e06d30
hika <hika00@gmail.com>
2024-04-20 15:25:20 +0900

    ,

--- packages/kore-db/src/DB.ts
+++ packages/kore-db/src/DB.ts
@@ -10,17 +10,25 @@ abstract class DB{
     #db:IDBDatabase|undefined;
     protected constructor(dbName:string, version:number = 1){
         this.#openAwait = new Promise((resolve, reject) => {
+            console.log("open")
             const request = indexedDB.open(dbName, version);
             request.onerror = e=>{
+                console.log("error", e)
                 this.onError(e);
                 reject(e);
             };
             request.onupgradeneeded = ()=>{
+                console.log("onupgrade")
                 this.#isFirst = true;
                 this.#db = request.result;
                 this.onCreate(
                     <TABLE extends Table<TABLE>>(tableClass:new () => TABLE):TableWrapper<TABLE>=>{
                         if(!this.#isFirst) exit("table() must be called on onCreate()");
+                        console.log("createObjectStore",
+                            tableClass.name,
+                            Table.keyPath(tableClass),
+                            Table.autoIncrement(tableClass)
+                        )
                         return new TableWrapper(
                             tableClass,
                             this.#db!.createObjectStore(tableClass.name, {
--- packages/kore-db/src/index.ts
+++ packages/kore-db/src/index.ts
@@ -1,1 +1,2 @@
-export {Table} from "./Table";
+export {Table, TableWrapper} from "./Table";
+export {DB} from "./DB";
