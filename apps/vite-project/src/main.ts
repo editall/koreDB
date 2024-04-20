98beca02b7893d6c25b45dae6495981e3d73433a
hika <hika00@gmail.com>
2024-04-20 15:45:01 +0900

    .

--- apps/vite-project/src/main.ts
+++ apps/vite-project/src/main.ts
@@ -3,6 +3,39 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
+class Member extends Table<Member> {
+    $rowid: number;
+    name: string;
+}
+class Club extends Table<Club> {
+    $rowid: number;
+    name: string;
+}
+class MemberClub extends Table<MemberClub> {
+    $rowid: number;
+    member_rowid: number;
+    club_rowid: number;
+}
+class TestDB extends DB {
+    constructor(dbName: string) {
+        super(dbName);
+    }
+    onCreate(
+        f: <TABLE extends Table<TABLE>>(tableClass: {
+            new (): TABLE;
+        }) => TableWrapper<TABLE>
+    ): void {
+        f(Member).index("name");
+        f(Club).index("name");
+        f(MemberClub).index("member_rowid").index("club_rowid");
+    }
+    async onInit(): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+    onError(e: Event): void {}
+}
+
+const testDB = new TestDB("test");
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
